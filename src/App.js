6cba34d16cd7d38abef07f3969795ee7d442e9ca
tranduchuynh <huynhtran924@gmail.com>
2020-05-27 08:59:00 +0700

    useEffect call api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.scss";
 
 import { useState } from "react";
 import Todolist from "./components/TodoList/Todolist";
+import TodoForm from "./components/TodoForm";
+import { useEffect } from "react";
+import PostList from "./components/PostList";
 
 function App() {
   const [todoList, setTodoList] = useState([
@@ -11,19 +14,52 @@ function App() {
     { id: 3, title: "They love my parent" },
   ]);
 
-  function handleTodoClick(todo) {
-    const index = todoList.findIndex((x) => x.id === todo.id);
-    if (index < 0) return;
+  const [postList, setPostList] = useState([]);
 
-    const newTodoList = [...todoList];
-    newTodoList.splice(index, 1);
-    setTodoList(newTodoList);
-  }
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const requestUrl =
+          "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+
+        const { data } = responseJSON;
+        setPostList(data);
+      } catch (error) {
+        console.log("Failed to fetch post list: ", error.message);
+      }
+    }
+    fetchPostList();
+  }, []);
+
+  // function handleTodoClick(todo) {
+  //   const index = todoList.findIndex((x) => x.id === todo.id);
+  //   if (index < 0) return;
+
+  //   const newTodoList = [...todoList];
+  //   newTodoList.splice(index, 1);
+  //   setTodoList(newTodoList);
+  // }
+
+  // function handleTodoFormSubmit(formValues) {
+  //   // add new todo to current todo list
+  //   console.log(formValues);
+  //   const newTodo = {
+  //     id: todoList.length + 1,
+  //     ...formValues,
+  //   };
+  //   const newTodoList = [...todoList];
+  //   newTodoList.push(newTodo);
+  //   setTodoList(newTodoList);
+  // }
 
   return (
     <div className="app">
       <h1>Welcome to react hooks</h1>
-      <Todolist todos={todoList} onTodoClick={handleTodoClick} />
+      {/* <TodoForm onSubmit={handleTodoFormSubmit} />
+      <Todolist todos={todoList} onTodoClick={handleTodoClick} /> */}
+      <PostList posts={postList} />
     </div>
   );
 }
